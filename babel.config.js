4f9065bac786dbe01772e6fdadca84e4aeb8a30c
Sameer Maurya <sameermaurya97044@gmail.com>
2026-07-16 11:21:19 +0530

    feat: add babel-plugin-transform-remove-console to strip console logs in production builds

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,13 @@
 module.exports = function(api) {
   api.cache(true);
+  const plugins = ['react-native-reanimated/plugin'];
+  
+  if (process.env.NODE_ENV === 'production' || process.env.BABEL_ENV === 'production') {
+    plugins.push('transform-remove-console');
+  }
+
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: plugins,
   };
 };
